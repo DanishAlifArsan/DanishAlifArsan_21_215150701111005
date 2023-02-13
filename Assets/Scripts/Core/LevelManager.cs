594f57a6170c5e9c9526809cf2e5ca282df5e08b
DanishAlifArsan <101310504+DanishAlifArsan@users.noreply.github.com>
2023-02-13 08:14:30 +0700

    create LevelManager

---
+++ Assets/Scripts/Core/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private GameObject tile;
+    [SerializeField] private float x;
+    [SerializeField] private float y;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        CreateLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreateLevel() {
+        
+        float tileSize = tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+
+        for (int i = 0; i <= x; i++)
+        {
+            for (int j = 0; j <= y; j++)
+            {
+                GameObject newTile =  Instantiate(tile);
+                newTile.transform.position = new Vector3(tileSize * i, tileSize * j, 0);
+            }
+        }
+    }
+}
