2ed22b9835201b9797c575a181ba29b5a4e37bba
DanishAlifArsan <101310504+DanishAlifArsan@users.noreply.github.com>
2023-02-13 18:26:47 +0700

    add Grid and TileScript

---
+++ Assets/Scripts/Core/TileScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    public Grid GridPosition { get; private set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Setup(Grid GridPosition) {
+        this.GridPosition = GridPosition;
+    }
+}
