9e952fd3d76f4193d4efc893702c21447720a533
DanishAlifArsan <101310504+DanishAlifArsan@users.noreply.github.com>
2023-02-15 12:47:22 +0700

    fix UI and update GameManager

--- Assets/Scripts/Map/GameManager.cs
+++ Assets/Scripts/Map/GameManager.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     public TowerButton TowerBtn { get; set; }
 
+    private int currency;
+
+    [SerializeField] private Text currencyText;
+
+    public int Currency { 
+        get {
+            return currency;
+        } 
+        set {
+            this.currency = value;
+            this.currencyText.text = value.ToString() + " $";
+        } 
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Currency = 5;
     }
 
     // Update is called once per frame
