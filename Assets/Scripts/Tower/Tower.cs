aa1cc9a58c791ce11818c3eac1a374481a76ce15
DanishAlifArsan <101310504+DanishAlifArsan@users.noreply.github.com>
2023-02-17 07:51:51 +0700

    update Tower

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -9,6 +9,8 @@ public class Tower : MonoBehaviour
     [SerializeField] private float shotCooldown;
 
     private SpriteRenderer spriteRend;
+    private Enemy targettedEnemy;
+    private Queue<Enemy> enemyQueue = new Queue<Enemy>();
 
     // Start is called before the first frame update
     private void Start()
@@ -19,10 +21,32 @@ public class Tower : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        
+        Attack();
+        Debug.Log(targettedEnemy);
     }
 
     public void Select() {
         spriteRend.enabled = !spriteRend.enabled;
+    }
+
+    private void OnTriggerEnter2D(Collider2D Collision) {
+        if (Collision.tag == "Enemy") {
+            targettedEnemy = Collision.GetComponent<Enemy>();
+            enemyQueue.Enqueue(Collision.GetComponent<Enemy>());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D Collision) {
+        if (Collision.tag == "Enemy") {
+            targettedEnemy = null;
+        }
+    }
+
+    private void Attack() {
+        if (targettedEnemy != null && enemyQueue.Count > 0)
+        {
+            Debug.Log("attacks");
+            targettedEnemy = enemyQueue.Dequeue();
+        }
     }
 }
