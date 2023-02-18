419e77628c3160256bb7fd8e913bf4964cb37d26
DanishAlifArsan <101310504+DanishAlifArsan@users.noreply.github.com>
2023-02-18 10:29:25 +0700

    create AudioManager

---
+++ Assets/Scripts/Core/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    private AudioSource audio;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
